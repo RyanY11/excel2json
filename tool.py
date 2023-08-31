cb86106afc071cf21bc5d39404782d8238014854
RyanY11 <36234986+RyanY11@users.noreply.github.com>
2023-08-31 19:45:38 +0800

    Add files via upload

---
+++ tool.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 31 15:54:59 2023
+
+@author: ryan
+
+"""
+
+import pandas as pd
+import streamlit as st
+import json
+import ast
+
+# 检查是否为list并转换
+def is_string_list(var):
+    try:
+        return isinstance(ast.literal_eval(var), list)
+    except (ValueError, SyntaxError):
+        return False 
+
+
+# JSON格式转换
+@st.cache_data
+def convert_df(data):
+    ex_file = pd.ExcelFile(data)
+    sheets = ex_file.sheet_names
+    ex_data = {}
+    
+    js_data_list = []
+    js_data_name = []
+    for sheet in sheets:
+        js_data_name.append(str(sheet))
+        ex_data[sheet] = ex_file.parse(sheet)
+        
+        ex_col_data = {}
+        for column in ex_data[sheet].columns:
+            ex_col_data[column] = ex_data[sheet][column].tolist()
+            processed_list = []
+            for element in ex_col_data[column]:
+                if is_string_list(element):
+                    element = ast.literal_eval(element)
+                processed_list.append(element)
+            ex_col_data[column] = processed_list
+                
+        json_data = json.dumps(ex_col_data, indent=4)
+        js_data_list.append(json_data)
+
+    return js_data_name,js_data_list
+
+
+# 页面绘制
+st.title('Excel转JSON工具')
+st.info('上传Excel，每一列会被处理为以列名为键，列内容列表为值的JSON格式文件')
+st.write(' ')
+st.text('注意！需要保证每个分表中，列名只有一行，多行列名会将非第一行内容认为是列值的一部分展示到列表中')
+st.write(' ')
+
+st.header('Step 1. 上传Excel表格')
+file = st.file_uploader('选择数据文件（仅.xlsx格式）')
+
+if file is not None:
+    js_name,js_data = convert_df(file)
+
+    st.header('Step 2. 下载JSON文件')
+    for i in range(len(js_name)):
+        dl_name = str(js_name[i]) + '.json'
+        st.download_button(label=js_name[i],
+                    data=js_data[i],
+                    file_name=dl_name,
+                    mime='json')
+
+    st.write('')
+    st.info('完成~')
+    st.balloons()
